afe53f6c1fab3601cb429a7cf12b1784af20c07f
hsengivc <hsengivc@gmail.com>
2021-01-04 22:34:49 +0530

    add header footer react bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
-
+import { Container } from "react-bootstrap";
+import { Header, Footer } from "./components";
 export const App = () => (
   <div className="App">
     <header className="App-header">
-      <p>Hi</p>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h2>sdsd</h2>
+        </Container>
+      </main>
+      <Footer />
     </header>
   </div>
 );
