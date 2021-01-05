198ffe33ef15ac914fc27b5fddbf1f677cce6690
hsengivc <hsengivc@gmail.com>
2021-01-05 22:31:22 +0530

    added routing, products, rating pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
 import React from "react";
+//@ts-ignore
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import { Header, Footer } from "./components";
+import { HomeScreen, ProductScreen } from "./screens";
+
 export const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <Header />
-      <main className="py-3">
-        <Container>
-          <h2>sdsd</h2>
-        </Container>
-      </main>
-      <Footer />
-    </header>
-  </div>
+  <Router>
+    <Header />
+    <main className="py-3">
+      <Container>
+        <Route path="/" component={HomeScreen} exact />
+        <Route path="/product/:id" component={ProductScreen} />
+      </Container>
+    </main>
+    <Footer />
+  </Router>
 );
