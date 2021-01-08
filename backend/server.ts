9d112f847acf1735ed05b518211931a3e7dc1680
hsengivc <hsengivc@gmail.com>
2021-01-08 22:28:48 +0530

    added error handler

--- backend/server.ts
+++ backend/server.ts
@@ -4,6 +4,7 @@ import "colorts/lib/string";
 
 import { connectDB } from "./config";
 import { productRoutes } from "./routes";
+import { errorHandler, notFound } from "./middleware";
 
 const app = express();
 dotenv.config();
@@ -15,6 +16,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
